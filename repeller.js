7a5b16a30e7849501a35a511deb80cb4914ddd1a
Mirae-Jung <74214040+Mirae-Jung@users.noreply.github.com>
2020-11-17 21:58:36 +0900

    change rect to ellipse

--- repeller.js
+++ repeller.js
@@ -7,9 +7,8 @@ class Repeller {
   display() {
     stroke(255);
     strokeWeight(2);
-    fill(127);
-    rectMode(CENTER)
-    rect(this.position.x, this.position.y, 200, 50);
+    fill(255);
+    ellipse(this.position.x, this.position.y, 50, 50);
   }
 
   repel(p) {
